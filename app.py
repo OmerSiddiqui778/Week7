e28da4bf0a3289fc57cc890c616c43d034a4dca3
OmerSiddiqui778 <omersiddiqui1939@outlook.com>
2026-05-07 13:37:56 +1000

    made another change in the app.py file

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 print("Hello World")
-print("Jenkins should run after making changes in this file")
+print("Jenkins should run after making changes in this file, and pipeline should be executed in the jenkins")
